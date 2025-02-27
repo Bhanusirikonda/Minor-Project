207acce2537dc57e68931cd7515607036beccd81
Bhanusirikonda <sbhanuprasad2004@gmail.com>
2025-02-27 23:59:32 +0530

    add route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,24 +9,26 @@ import OperatorHome from './components/operator/OperatorHome';
 import AdminLogin from './components/admin/Admin'
 import AdminHome from './components/admin/AdminHome';
 import OwnerHome from './components/owner/OwnerHome'
-import EmployeeProfile from './components/user/EmployeeProfile';
+import EmployeeProfile from './components/Employee/EmployeeProfile';
+import EmployeeRegistration from './components/Employee/EmployeeRegistration';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<RootLayout />}>
-        <Route path="ownerLogin" element={<OwnerLogin />} />
-        <Route path="operatorLogin" element={<OperatorLogin />} />
-        <Route path="operatorRegister" element={<OperatorRegister />} />
-
-        <Route path="adminLogin" element={<AdminLogin/>}/>
-        </Route>
+          <Route path="ownerLogin" element={<OwnerLogin />} />
+          <Route path="operatorLogin" element={<OperatorLogin />} />
+          <Route path="operatorRegister" element={<OperatorRegister />} />
+          <Route path="adminLogin" element={<AdminLogin/>}/>
+          </Route>
         <Route path="adminHome" element={<AdminHome/>}/>
-        <Route path="ownerHome" element={<OwnerHome/>}/>
+        <Route path="ownerHome" element={<OwnerHome/>}>
+        </Route>
+        <Route path="employeeRegistration" element={<EmployeeRegistration/>} />
         <Route path="operatorHome" element={<OperatorHome/>}/>
         <Route path="employee/:id" element={<EmployeeProfile/>}/>
-
+        
       </Routes>
     </Router>
   );

a715991e9822c1692304d6e5823addbe6872a9f8
Bhanusirikonda <sbhanuprasad2004@gmail.com>
2025-02-28 23:16:12 +0530

    add footer

---
+++ frontend/src/components/rootlayout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div><footer className="py-3  text-white mt-auto">
+    <div className="container">
+      <p className="text-center mb-0">© 2025 AS HRM Services. All rights reserved.</p>
+    </div>
+  </footer></div>
+  )
+}
+
+export default Footer

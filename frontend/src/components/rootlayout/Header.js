2abd1dc7951f488970386b7beeada381d439612c
Bhanusirikonda <sbhanuprasad2004@gmail.com>
2025-02-28 23:16:30 +0530

    add header

---
+++ frontend/src/components/rootlayout/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+         <header className="py-4  text-white">
+        <div className="container">
+      <h1 className="text-center display-4 fw-bold mb-2">AS HRM SERVICES</h1>
+      <p className="text-center lead mb-0">BLUE DART EXPRESS LTD-HYDERABAD</p>
+    </div>
+  </header>
+  </div>
+  )
+}
+
+export default Header
